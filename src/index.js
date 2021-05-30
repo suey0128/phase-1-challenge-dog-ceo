cbbedaa05c79152abfeefa18e2569080e0478b54
suey0128 <syui0128@gmail.com>
2021-05-30 10:58:12 -0700

    Completed assignment clean ver.

--- src/index.js
+++ src/index.js
@@ -1,78 +1,82 @@
-addEventListener("DOMContentLoaded", (e) => { console.log('fullyloaded')});
+addEventListener("DOMContentLoaded", (e) => { 
+    console.log('fullyloaded')
+    getDogPhoto()
+    .then(data => data['message'].forEach(dogPhotoUrl => displayPhoto(dogPhotoUrl)))
+    .catch(error => console.error('Error: ', error))
+    dogBreed() 
+})
 console.log('%c HI', 'color: firebrick')
+
 
 
 //Challenge 1 
 //fetch data via jsons
-function dogPhoto(url) {
-    return fetch(url)
+function getDogPhoto() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4')
     .then(res => res.json())
-    // .then(data => console.log(data['message']))
-    .then(data => data['message'].forEach(displayPhoto))
-    .catch(error => console.error('Error: ', error))
+    
 }
 
-dogPhoto('https://dog.ceo/api/breeds/image/random/4');
 
 // function that display the photo every photo
-function displayPhoto (element) {
+function displayPhoto (dogPhotoUrl) {
     let img = document.createElement('img');
-    img.setAttribute('src',element);
+    img.setAttribute('src',dogPhotoUrl);
     document.querySelector('#dog-image-container').append(img);
 }
 
 
 //Challenge 2
 //fetch data via jsons
-function dogBreed(url) {
-    return fetch(url)
+function dogBreed() {
+    return fetch('https://dog.ceo/api/breeds/list/all')
     .then(res => res.json())
     .then(data => Object.keys(data['message']).map(displayBreed))
     .catch(error => console.error('Error: ', error))
 }
 
-dogBreed('https://dog.ceo/api/breeds/list/all');
 
-//function that put the breeds in the page
-let li;
-let ul;
-function displayBreed (elem) {
-    li = document.createElement('li');
-    li.textContent = elem;
-    li.className = 'item';
+//function that put the breeds on the page
+function displayBreed (dogBreed) {
+    const li = document.createElement('li');
+    li.textContent = dogBreed;
+    li.className = 'item'
     document.querySelector('#dog-breeds').append(li)
-
-    li.addEventListener('click', e => e.target.style.color = 'red');
-    // //Challenge 3
-    // ul = document.querySelectorAll('.item');
-    // for (sublist of ul) {
-    //     sublist.addEventListener('click', e => e.target.style.color = 'red')};
-
-
 }
 
 
+//add event listener to make the dog breed name red
+document.querySelector('#dog-breeds').addEventListener('click', (e) => {
+        e.target.style.color = 'red'
+})
 
-//Challenge 4
-function onChange () {
-    let breed = document.getElementById('breed-dropdown'); 
-    let letter = breed.value;
-    ul = document.querySelectorAll('.item');
+
+//Challenge 4, select breeds by the starting letter with add event listener
+document.querySelector('#breed-dropdown').addEventListener('change', (e) => {
+    const ul = document.querySelectorAll('#dog-breeds li')
     for (sublist of ul) {
-        if (sublist.textContent.startsWith(`${letter}`) === false) {
+        if (sublist.textContent.startsWith(`${e.target.value}`) === false) {
         sublist.style.display = 'none'
-       } else { sublist.style.display = ''}
+        } else { sublist.style.display = ''}
     }
-}
+})
 
 
-function fliterBreed () {
-    document.getElementById('breed-dropdown').setAttribute('onchange','onChange()');
-    }
-
-fliterBreed ();
-
-// document.addEventListener('DOMContentLoaded', function fliterBreed () {
-//     document.getElementById('breed-dropdown').setAttribute('onchange','onChange()');
-//     fliterBreed ();
+// //Challenge 4, select breeds by the starting letter with onchange attribute
+// function onChange() {
+//     const breed = document.getElementById('breed-dropdown'); 
+//     const letter = breed.value;
+//     const ul = document.querySelectorAll('#dog-breeds li')
+//     for (sublist of ul) {
+//         if (sublist.textContent.startsWith(`${letter}`) === false) {
+//         sublist.style.display = 'none'
+//        } else { sublist.style.display = ''}
+//     }
 // }
+
+
+// function fliterBreed () {
+//     document.getElementById('breed-dropdown').setAttribute('onchange','onChange()');
+//     }
+
+// fliterBreed ();
